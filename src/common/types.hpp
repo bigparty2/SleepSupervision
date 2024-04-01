6e5608b1e62378d3a5bd87c08dc66888a3a70717
Kauê Gomes <kauesgomes22@gmail.com>
2024-04-01 19:11:57 -0300

    Tipo para listas de computadores

--- src/common/types.hpp
+++ src/common/types.hpp
@@ -1,10 +1,16 @@
 #ifndef TYPES_HPP
 #define TYPES_HPP
 
+#include <vector>
+#include "computer/computer.hpp"
+
 namespace ss
 {
     //Abstração do tipo byte
     typedef unsigned char byte;
+
+    // Abstração para lista de computadores
+    typedef std::vector<ss::computer> computers;
 }
 
 #endif //TYPES_HPP
